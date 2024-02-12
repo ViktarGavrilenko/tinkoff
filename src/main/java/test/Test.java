271dfcdb9d2cb592e37d74d3b3fb4e1c2c9959be
Viktor Gavrilenko <viktargavrilenko@gmail.com>
2024-02-12 09:24:50 +0300

    Correct third task

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -1,22 +1,12 @@
 package test;
 
-import java.util.List;
+import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
-        List<Integer> numbers = List.of(1, 2, 3, 4);
-        int total = 0;
-
-        for (Integer x : numbers) {
-            if (x % 2 == 0) total += x * x;
-        }
-        System.out.println(total);
-
-        //int t= numbers.stream().filter(x->x%2==0).collect(Collections.toInt());
-        //int t= numbers.stream().mapToInt(x->{if (x%2==0) return x*x}).sum();
-        int t = numbers.stream().mapToInt(x -> x % 2 == 0 ? x * x : 0).sum();
-        System.out.println(t);
-        Test test = new Test();
-        test.hashCode();
+        Scanner scanner = new Scanner(System.in);
+        int first = Integer.parseInt(scanner.nextLine());
+        int second = Integer.parseInt(scanner.nextLine());
+        System.out.println(first + second);
     }
 }
